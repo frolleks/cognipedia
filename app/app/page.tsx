4340698cda4d2141f0f3dd1cdd0f238cd838358b
frolleks <spaceactive.akbar@gmail.com>
2025-11-08 20:53:52 +0700

    feat: add source code link at the bottom of the homepage

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ArrowUp } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
+    <div className="flex flex-col min-h-screen items-center justify-between font-sans">
+      <div></div>
       <main>
         <div className="grid gap-y-4">
           <p className="text-center font-semibold text-2xl">cognipedia</p>
@@ -22,6 +24,15 @@ export default function Home() {
           </form>
         </div>
       </main>
+      <footer className="mb-8">
+        <ul className="flex items-center gap-2">
+          <Link href="https://github.com/frolleks/cognipedia">
+            <li className="text-muted-foreground hover:underline">
+              source code
+            </li>
+          </Link>
+        </ul>
+      </footer>
     </div>
   );
 }
